d40d29e977cdf78fb86316e7d7aae3349d20dfe2
Jordan Burch <jordanburch101@gmail.com>
2020-05-13 20:30:28 -0500

    added checkLetter func

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const phrase = document.getElementById('phrase');
 const overlay = document.getElementById('overlay');
 const ul = document.querySelector('#phrase ul');
 const startButton = document.querySelector('.btn__reset'); // Start game button
+const scoreBoard = document.querySelector('#scoreboard ol');
 let missed = 0; // Hearts of the player
 
 const phrases = [
@@ -13,10 +14,12 @@ const phrases = [
     "rain on your parade",
 ];
 
+// Remove overplay after clicking start
 startButton.addEventListener('click', (e) => {
     overlay.style.display = 'none';
 });
 
+// Choose one of the phrases from the phrases array
 function getRandomPhraseAsArray(arr) {
     function getRandomArray (arr) {
         const num = Math.floor((Math.random() * phrases.length));
@@ -27,18 +30,42 @@ function getRandomPhraseAsArray(arr) {
     return string;
 }
 
+// Convert the chosen phrase into its own array of strings
 function addPhraseToDisplay(arr) {
     const phraseArray = getRandomPhraseAsArray(arr);
-    for(let i = 0; i < arr.length; i += 1) {
+    for(let i = 0; i < phraseArray.length; i += 1) {
         const li = document.createElement ('li');
-        if (phraseArray.textContent != ' ') {
-            li.textContent = `${i}`;
-            li.className = 'letter';
+        if (phraseArray[i] === ' ') {
+            li.innerHTML = '';
+            li.className = 'space';
             ul.appendChild(li);
         } else {
-
+            li.textContent = phraseArray[i];
+            li.className = 'letter';
+            ul.appendChild(li)
         }
     }
 }
+
+
+function checkLetter(key) {
+    const li = document.getElementsByClassName('letter');
+    for(let i = 0; i < li.length; i += 1) {
+        if(li[i].textContent === key) {
+            li[i].className += ' show';
+            return li[i];
+        } else {
+            //return null;
+        }
+    }
+    
+}
+
+qwerty.addEventListener('click', (e) => {
+    const letter = e.target;
+    checkLetter(letter);
+});
+
+
+// For testing
 addPhraseToDisplay(phrases);
-console.log(getRandomPhraseAsArray(phrases));
